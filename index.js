7b536f25a36bb40dcbeb23ef9188a95572c0f8e0
Vinicius <86333257+viniflopes@users.noreply.github.com>
2021-08-09 11:17:48 -0300

    Utilizando this, if, return e privando acesso ao código

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 class Cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
-    rg;
 }
+class contaCorrente {
+    agencia;
+    _saldo = 0;
 
+    sacar(valor){
+        if (this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor<=0){
+            return;
+        }
+            this._saldo += valor;
+    }
+}
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-const cliente3 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
-cliente1.rg = 256853369;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-cliente3.nome = "Luis";
-cliente3.cpf = 88824533309;
-cliente3.agencia = 1001;
-cliente3.saldo = 0;
+const contaCorrenteRicardo = new contaCorrente
+contaCorrenteRicardo.agencia = 1001;
 
-console.log(cliente1, cliente2, cliente3);
+contaCorrenteRicardo.depositar(-100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
+
+console.log(contaCorrenteRicardo);
