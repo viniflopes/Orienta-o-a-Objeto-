72a0ca16b1ab7e2a5c089b464b14edb5d8002d3c
Vinicius <86333257+viniflopes@users.noreply.github.com>
2021-08-10 10:53:31 -0300

    Adicionando Poupança

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 import {Cliente} from "./Cliente.js"
-
 import {contaCorrente} from "./ContaCorrente.js"
+import {ContaPoupanca} from "./ContaPoupanca.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
-const cliente2 = new Cliente("Alice", 88822233309);
-let numeroDeContas = 0;
+
 const contaCorrenteRicardo = new contaCorrente(cliente1, 1001);
 
 contaCorrenteRicardo.depositar(500);
-const conta2 = new contaCorrente(cliente2, 102);
+contaCorrenteRicardo.sacar(100);
 
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001)
 
-contaCorrenteRicardo.transferir(200, conta2)
-
-console.log(contaCorrente.numeroDeContas);
+console.log(contaPoupanca);
+console.log(contaCorrenteRicardo);
