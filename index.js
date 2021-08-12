9a671b79ed9d042cdeedb8d9efac6346926284b7
Vinicius <86333257+viniflopes@users.noreply.github.com>
2021-08-12 10:34:00 -0300

    Compreendendo super e sobescrevendo comportamentos

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import {Cliente} from "./Cliente.js"
-import {contaCorrente} from "./ContaCorrente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 import {ContaPoupanca} from "./ContaPoupanca.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
-const contaCorrenteRicardo = new contaCorrente(cliente1, 1001);
-
+const contaCorrenteRicardo = new ContaCorrente(0, cliente1, 1001);
 contaCorrenteRicardo.depositar(500);
 contaCorrenteRicardo.sacar(100);
 
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001)
+contaPoupanca.sacar(10);
 
 console.log(contaPoupanca);
 console.log(contaCorrenteRicardo);
