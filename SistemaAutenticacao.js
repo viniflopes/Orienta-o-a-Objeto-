75e14da06cd4dab5365d9d8c6ecc0452436c69b4
Vinicius <86333257+viniflopes@users.noreply.github.com>
2021-08-13 11:49:44 -0300

    Duck typing

--- SistemaAutenticacao.js
+++ SistemaAutenticacao.js
@@ -1,5 +1,12 @@
 export class SistemaAutenticacao{
-    static login(funcionario, senha){
-        return funcionario.senha == senha;
+    static login(autenticavel, senha){
+        if(SistemaAutenticacao.ehAutenticavel(autenticavel)){
+        return autenticavel.autenticar(senha);
+        }
+    return false
+    }
+    static ehAutenticavel(autenticavel){
+        return "autenticar" in autenticavel && 
+        autenticavel.autenticar instanceof Function;
     }
 }
--- index.js
+++ index.js
@@ -4,10 +4,15 @@ import { Diretor } from "./Funcionario/Diretor.js"
 import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 const diretor = new Diretor("Rodrigo", 10000, 2356895867);
-diretor.cadastrarSenha("123456");
+diretor.cadastrarSenha("321");
 const gerente = new Gerente("Laura", 5000, 2415636298);
 gerente.cadastrarSenha("123");
 
-const estaLogado = SistemaAutenticacao.login(gerente, "123");
+const cliente = new Cliente("Luiza", 16256985487, "2209");
 
-console.log(estaLogado);
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "321");
+
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "2209");
+
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
